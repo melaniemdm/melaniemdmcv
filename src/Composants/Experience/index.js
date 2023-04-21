640d52ba693a39008fb1624d94fde5dc6447af20
melaniemdm <melanieborja.pro@gmail.com>
2023-04-21 16:15:13 +0200

    fix(css) print refacto(all)

--- src/Composants/Experience/index.js
+++ src/Composants/Experience/index.js
@@ -2,16 +2,18 @@ import  Dates  from '../Dates';
 import Liste from '../Liste';
 import SousTitle from '../SousTitle';
 import TitleSection from '../TitleSection';
-
+import './style.scss';
 export default function Experience({work}){
   const listItemsWorks = work.map((workItem, index )=>   <div key={index}> 
-       <Dates dateStart={workItem.startDate} dateEnd={workItem.endDate}/> 
+  <div className='experienceItems'>
+       <div className='experienceItemsBloc1'><Dates dateStart={workItem.startDate} dateEnd={workItem.endDate}/> 
        <SousTitle sousTitle={workItem.name}/>
-       <SousTitle sousTitle={workItem.location}/>
+       <SousTitle sousTitle={workItem.location}/></div>
+       <div className='experienceItemsBloc2'>
       <Liste texteList={workItem.summary}/>
       Points forts :
-      <Liste texteList={workItem.highlights}/>
-
+      <Liste texteList={workItem.highlights}/></div>
+</div>
    </div> )
     return(<div className="experience">
     <TitleSection icon="group_add" titleSection="Experiences" />
--- src/pages/Cv.js
+++ src/pages/Cv.js
@@ -6,30 +6,27 @@ import Contact from "../Composants/Contact";
 import Expertise from "../Composants/Expertise";
 import Education from "../Composants/Education";
 import Profil from "../Composants/Profil";
-import './style.scss';
+import "./style.scss";
 import Header from "../Composants/Header";
 export default function Cv() {
   return (
     <div className="containerCv">
-      <Header basics={data.basics}/>
+      <Header basics={data.basics} />
       <div className="containerBloc">
- <div className="bloc2">   
-   <Contact data={data.basics}/>
-      <Profil profil={data.basics.summary} />
-      <Education education={data.education} />
-      <Expertise skills={data.skills} />
-      <Interets interets={data.interests} />
-      </div>
-
-      <div className="bloc1">
-   
-    
-      <Experience work={data.work} />
-     
-      </div>
-
-     
-      
+        <div className="bloc1">
+          <Contact className="contactItem" data={data.basics} />
+          <Profil className="profilItem" profil={data.basics.summary} />
+        </div>
+        <div className="bloc3">
+          <Experience work={data.work} />
+        </div>
+        <div className="bloc4">
+          <div className="bloc2">
+            <Education education={data.education} />
+            <Interets interets={data.interests} />
+          </div>
+          <Expertise skills={data.skills} />
+        </div>
       </div>
     </div>
   );
